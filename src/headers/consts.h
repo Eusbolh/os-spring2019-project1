688a8e9b290733847001ef48962aa69e938be2b8
Alper Toygar <alpertoygar@yandex.com>
2019-03-28 21:41:56 +0300

    al

--- src/headers/consts.h
+++ src/headers/consts.h
@@ -1,4 +1,5 @@
 #define TEMP_FILE_NAME "tmp.txt"
-#define CRON_FILE_NAME "cron.txt"
+#define CRON_FILE_NAME "/tmp/shelldon_cron.txt"
+#define KERNEL_MODULE "llamas"
 
 #define MAX_LINE       4096              /* 4096 chars per line, per command, should be enough. */
