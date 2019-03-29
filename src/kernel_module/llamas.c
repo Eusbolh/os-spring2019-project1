a69ddd9e23d37441e57d115a7b6f3ec984e8464f
Cüneyt Emre Yavuz <cyavuz15@ku.edu.tr>
2019-03-29 11:10:10 +0300

    kernel module is updated for recursive calls

--- src/kernel_module/llamas.c
+++ src/kernel_module/llamas.c
@@ -5,53 +5,71 @@
 #include <linux/types.h>
 #include <linux/slab.h>
 #include <linux/moduleparam.h>
-
 #include <linux/sched/signal.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Emre Yavuz");
 
-int processID=0; 
+int processID=0;
+
+static void recurOldestChild(struct task_struct* proc) {
+
+	/* Iterating over children to find oldest child */
+	u64 start_time_var = -1;																/* Variable to compare next children with current oldest child - default value is '-1' because start time of a process cannot be less than zero */
+	struct task_struct* oldest_child = NULL;								/* Variable to keep oldest child to print its info at the end */
+	struct task_struct* child;															/* Variable to keep children in list_for_each loop */
+	struct list_head* children;															/* Linked list head of children of process */
+	list_for_each(children, &proc->children) {
+		/* 'child' is now current child in the linked list */
+		child = list_entry(children, struct task_struct, sibling);
+		
+		/* Recursive call to child */
+		recurOldestChild(child);
+
+		/* Comparing child's start_time with current oldest child */
+		if (start_time_var == -1) {
+			start_time_var = child->start_time;
+			oldest_child = child;
+		}
+		else if (child->start_time < start_time_var) {
+			start_time_var = child->start_time;
+			oldest_child = child;
+		} 
+	}
+
+	/* If oldest_child is still null, it means that &proc->children is empty and code didn't react into 'list_for_each' loop */
+	/* Otherwise, we found our oldest child */
+	if (oldest_child != NULL) {
+		printk(KERN_ALERT "Process(ID/Name): %d/%s - Oldest Child(ID/Name): %d/%s", proc->pid, proc->comm, oldest_child->pid, oldest_child->comm);
+	}
+}
+
 static int llamas_init(void)
 {
+	struct task_struct* task;																/* Root of process tree */
+	bool pid_flag = true;																		/* Boolean to keep track of whether we found a process with given id */
+
+	/* Kernel alert to indicate that kernel module is loaded */
 	printk(KERN_ALERT " \n");
 	printk(KERN_ALERT "Llamas Kernel Module is loaded.\n");
-	struct task_struct* task;
-	bool pidFlag = true;
-	bool childrenFlag = true;
-	u64 start_time_var = -1;
-	struct task_struct* oldest_child = NULL;
+	
 	for_each_process(task){
 		if (task->pid == processID) {
+
+			/* We found a process with given id, now we can update the flag */
+			pid_flag = false;
+
+			/* Kernel alert to indicate that a process is found with given id */
 			printk(KERN_ALERT "Process with id %d is found: %s", processID, task->comm);
-			pidFlag = false;
-			struct task_struct* child;
-			struct list_head* children;
-			list_for_each(children, &task->children) {
-				childrenFlag = false;
-				child = list_entry(children, struct task_struct, sibling);
-				if (start_time_var == -1) {
-					start_time_var = child->real_start_time;
-					oldest_child = child;
-				}
-				else if (child->real_start_time < start_time_var) {
-					start_time_var = child->real_start_time;
-					oldest_child = child;
-				} 
-				printk(KERN_ALERT "Child Info: %d/%llu/%s\n", child->pid, child->real_start_time, child->comm);
-			}
+
+			/* Calling 'recurOldestChild' to find oldest children of processes in subtree */
+			recurOldestChild(task);
 		}
 	}
-	if (pidFlag) {
+
+	/* If pid_flag is still true, it means that we couldn't find a process with given id */
+	if (pid_flag) {
 		printk(KERN_ALERT "No process with id %d is found.", processID);
-	}
-	if (childrenFlag) {
-		printk(KERN_ALERT "No child is found for process %d", processID);
-	}
-	else {
-		if (oldest_child != NULL) {
-			printk(KERN_ALERT "Oldest child: %d/%s", oldest_child->pid, oldest_child->comm);
-		}
 	}
 	printk(KERN_ALERT " \n");
 	return 0;
@@ -59,6 +77,7 @@ static int llamas_init(void)
  
 static void llamas_cleanup(void)
 {
+	/* Kernel alert to indicate that kernel module is unloaded */
 	printk(KERN_ALERT "Llamas Kernel Module is unloaded.\n");
 }
 
